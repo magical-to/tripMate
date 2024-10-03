37e8b8836f8f497f1dccbb2de057ce1ee2724628
choeyunseo <choeyunseo4060@naver.com>
2024-10-04 00:51:03 +0900

    Add Modal Componente

---
+++ tripMateApp/src/Components/Modal.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Modal.css';
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-container" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-close-button" onClick={onClose}>
+          &times;
+        </button>
+        <div className="modal-content">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
