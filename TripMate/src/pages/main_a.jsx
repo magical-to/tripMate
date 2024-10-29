cb33534f912f13b5cc4b98641f98d28cfd7f8bb9
JEONG-JIN-GYO <202021035@sangmyung.kr>
2024-10-29 10:06:20 +0900

    Delete TripMate directory

--- TripMate/src/pages/main_a.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App_A from "../App_a.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App_A />
-  </React.StrictMode>
-);
