acd60ac9550ae59a27ea01cdb887821f3271dc6f
JEONG-JIN-GYO <202021035@sangmyung.kr>
2024-11-02 15:16:28 +0900

    Update Home.jsx

--- tripMateApp/src/Pages/Home.jsx
+++ tripMateApp/src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { validateFriendId } from '../Services/authService'; 
-import Slider from 'react-slick'; // react-slick 임포트
+import Slider from 'react-slick';
 import Header from '../Components/Header';
 import Button from '../Components/Button';
-import 'slick-carousel/slick/slick.css'; // slick 기본 CSS
-import 'slick-carousel/slick/slick-theme.css'; // slick 테마 CSS
+import SmallChatComponent from '../Components/SmallChatComponent';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './Home.css'; 
 
 import Map1 from '../assets/1.png';
@@ -19,7 +20,6 @@ const slideImages = [Map1, Map2, Map3, Map4, Map5];
 const Home = () => {
   const navigate = useNavigate();
 
-  // react-slick 슬라이더 설정
   const settings = {
     dots: true,
     infinite: true,
@@ -29,36 +29,39 @@ const Home = () => {
     autoplay: true,
     autoplaySpeed: 5000,
   };
-  // PLAN 버튼 클릭 시 호출되는 함수 
+
   const handlePlanClick = async () => {
-    const token = localStorage.getItem('access_token'); // 로컬 스토리지에서 토큰 가져오기
+    const token = localStorage.getItem('access_token');
   
     if (token) {
-      // 토큰이 존재할 경우, validateFriendId를 사용해 유효한 사용자 확인
       const isUserValid = await validateFriendId(token);
   
       if (isUserValid) {
-        // 유효한 사용자일 경우 go 페이지로 이동
         navigate('/go');
       } else {
-        // 유효하지 않은 사용자일 경우 경고 메시지 표시 후 로그인 페이지로 이동
         alert('사용자가 유효하지 않습니다. 로그인 페이지로 이동합니다.');
         navigate('/login');
       }
     } else {
-      // 토큰이 없을 경우 경고 메시지 표시 후 로그인 페이지로 이동
       alert('로그인 후 이용 가능합니다. 로그인 페이지로 이동합니다.');
       navigate('/login');
     }
   };
 
+  const handleNavigateToMap = () => {
+    navigate('/map');
+  };
+
   return (
     <div>
+      <SmallChatComponent />
       <Header />
       <div className="home-container">
         <div className="home-left">
           <h1>여행 준비의 모든 것, TripMate와 함께!</h1>
-          <p>간편하게 여행 계획을 세워보세요.</p>
+          <p onClick={handleNavigateToMap} className="clickable-text">
+            간편하게 여행 계획을 세워보세요.
+          </p>
           <div className="button-container">
             <Button 
               text="PLAN!!" 
@@ -69,7 +72,6 @@ const Home = () => {
         </div>
 
         <div className="home-right">
-          {/* 슬라이더 컴포넌트 */}
           <Slider {...settings}>
             {slideImages.map((image, index) => (
               <div key={index}>
