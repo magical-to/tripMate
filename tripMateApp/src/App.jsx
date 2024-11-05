299dbc4d887b7c4ef22b29770b497057a05a4829
JEONG-JIN-GYO <202021035@sangmyung.kr>
2024-11-05 14:20:57 +0900

    Update App.jsx

--- tripMateApp/src/App.jsx
+++ tripMateApp/src/App.jsx
@@ -5,9 +5,9 @@ import Home from './Pages/Home';
 import Login from './Pages/Login';
 import SignUp from './Pages/Signup';
 import Go from './Pages/Go';
+import Chat from './Pages/Chat';
 import Mytrip from './Pages/Mytrip';
-import Plan from './Pages/Plan';
-import Chat from './Components/Chat';
+import Map from './Pages/Map';
 
 const App = () => {
   return (
@@ -17,9 +17,9 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/go" element={<Go />} />
-        <Route path="/mytrip" element={<Mytrip />} />
-        <Route path="/plan" element={<Plan />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/mytrip" element={<Mytrip />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </BrowserRouter>
   );
