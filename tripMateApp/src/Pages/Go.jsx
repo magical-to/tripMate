ccbf225147e39411f9f1f6b160c5cdeabd12562e
choeyunseo <choeyunseo4060@naver.com>
2024-11-08 19:49:55 +0900

    Add createTrip api

--- tripMateApp/src/Pages/Go.jsx
+++ tripMateApp/src/Pages/Go.jsx
@@ -6,6 +6,7 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './Go.css';
 import { validateFriendId } from '../Services/authService'; // API 함수 가져오기
+import { createTrip, inviteFriend } from '../Services/tripService'; // API 함수 가져오기
 
 export default function Go() {
     const navigate = useNavigate();
@@ -17,7 +18,8 @@ export default function Go() {
     const [startTime, setStartTime] = useState('');
     const [endTime, setEndTime] = useState('');
     const [friendId, setFriendId] = useState('');
-    const [friendsList, setFriendsList] = useState(['jjang', 'yoon', 'choi']);
+    const [validatedFriends, setValidatedFriends] = useState([]); // 검증된 친구 아이디 저장
+    const [tripId, setTripId] = useState(null); // tripId 상태 변수 추가
     const [error, setError] = useState('');
 
     const handleOpenModal = (solo) => {
@@ -28,15 +30,52 @@ export default function Go() {
 
     const handleCloseModal = () => setIsModalOpen(false);
 
-    const handleNextStep = () => {
-        if (isSolo && step === 3) {
-            setStep(5);
-        } else {
+    const handleNextStep = async () => {
+        // 여행 생성
+        if (!isSolo && step === 3) {
+            console.log("3단계");
+            try {
+                const tripResponse = await createTrip(title, selectedRange, startTime, endTime);
+                console.log(tripResponse.data);
+                setTripId(tripResponse.data.trip_id); // tripId 상태에 저장
+                console.log('여행 생성 성공:', tripResponse.id);
+                setStep(step + 1);
+            } 
+            catch (error) {
+                setError(error.message); // 에러 처리
+            }
+        } 
+
+        // 친구 초대 
+        else if (!isSolo && step === 4) {
+            console.log("4단계");
+            try {
+                await inviteFriend(validatedFriends, tripId); // tripId와 함께 친구 초대
+                setStep(step + 1);
+            } 
+            catch (error) {
+                setError(error.message); // 에러 처리
+            }
+        } 
+        
+        else {
             setStep(step + 1);
         }
     };
 
     const handlePreviousStep = () => step > 1 && setStep(step - 1);
+
+    const handleAddFriend = async () => {
+        setError('');
+        try {
+            const data = await validateFriendId(friendId); // 친구 아이디 검증
+            setValidatedFriends([...validatedFriends, friendId]); // 검증된 친구 아이디 추가
+            setFriendId(''); // 입력 필드 초기화
+        } catch (error) {
+            alert('존재하지 않는 ID입니다.'); // 경고창으로 메시지 표시
+            setError(error.message); // 에러 처리 (필요시)
+        }
+    };
 
     useEffect(() => {
         if (step === 5) {
@@ -47,24 +86,11 @@ export default function Go() {
         }
     }, [step]);
 
-    const handleAddFriend = async () => {
-        setError('');
-        try {
-            const data = await validateFriendId(friendId);
-            setFriendsList([...friendsList, friendId]);
-            setFriendId('');
-            setStep(step + 1);
-        } catch (error) {
-            setError(error.message);
-        }
-    };
-
     return (
         <div className="go-container">
-            <Header /> {/* Header 컴포넌트 추가 */}
+            <Header />
 
             <div className="content">
-                {/* 버튼 컨테이너 */}
                 <div className="button-container">
                     <button onClick={() => handleOpenModal(true)}>혼자 떠나요!</button>
                     <button onClick={() => handleOpenModal(false)}>같이 떠나요!</button>
@@ -72,107 +98,106 @@ export default function Go() {
 
                 {isModalOpen && (
                     <Modal
-                    customClass="step-modal"
-                    onClose={handleCloseModal}
-                    onBack={handlePreviousStep}
-                    onNext={handleNextStep}
-                >
-                    {step === 1 && (
-                        <>
-                            <h2>STEP 1: 여행 제목을 입력하세요</h2>
-                            <input
-                                type="text"
-                                placeholder="여행 제목을 입력하세요"
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
-                            />
-                            <div className="modal-buttons">
-                                <button className="next-button" onClick={handleNextStep}>
-                                    다음
-                                </button>
-                            </div>
-                        </>
-                    )}
-                
-                    {step === 2 && (
-                        <>
-                            <h2>STEP 2: 여행 일정을 선택하세요</h2>
-                            <Calendar
-                                className="calender-custom"
-                                selectRange={true}
-                                onChange={setSelectedRange}
-                                value={selectedRange}
-                            />
-                            <div className="modal-buttons">
-                                <button className="back-button" onClick={handlePreviousStep}>
-                                </button>
-                                <button className="next-button" onClick={handleNextStep}>
-                                    다음
-                                </button>
-                            </div>
-                        </>
-                    )}
-                
-                {step === 3 && (
-                    <div className="time-container">
-                        <h2>STEP 3: 여행 시간을 설정하세요</h2>
-                        <input
-                        type="time"
-                        value={startTime}
-                        onChange={(e) => setStartTime(e.target.value)}
-                        />
-                        <input
-                         type="time"
-                         value={endTime}
-                         onChange={(e) => setEndTime(e.target.value)}
-                         />
-                         <div className="modal-buttons">
-                            <button className="back-button" onClick={handlePreviousStep}>
-                            </button>
-                            <button className="next-button2" onClick={handleNextStep}>
-                                다음
-                                </button>
+                        customClass="step-modal"
+                        onClose={handleCloseModal}
+                        onBack={handlePreviousStep}
+                        onNext={handleNextStep}
+                    >
+                        {step === 1 && (
+                            <>
+                                <h2>STEP 1: 여행 제목을 입력하세요</h2>
+                                <input
+                                    type="text"
+                                    placeholder="여행 제목을 입력하세요"
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                />
+                                <div className="modal-buttons">
+                                    <button className="next-button" onClick={handleNextStep}>
+                                        다음
+                                    </button>
                                 </div>
-                                </div>
-                            )}
+                            </>
+                        )}
 
-                    {!isSolo && step === 4 && (
-                        <>
-                            <h2>STEP 4: 친구를 초대하세요</h2>
-                            <input
-                                type="text"
-                                placeholder="친구 아이디 입력"
-                                value={friendId}
-                                onChange={(e) => setFriendId(e.target.value)}
-                            />
-                            <button onClick={handleAddFriend}>친구 추가</button>
-                            {error && <p className="error-message">{error}</p>}
-                            <ul>
-                                {friendsList.map((friend, index) => (
-                                    <li key={index}>{friend}</li>
-                                ))}
-                            </ul>
-                            <div className="modal-buttons">
-                                <button className="back-button" onClick={handlePreviousStep}>
-                                </button>
-                                <button className="next-button" onClick={handleNextStep}>
-                                    다음
-                                </button>
+                        {step === 2 && (
+                            <>
+                                <h2>STEP 2: 여행 일정을 선택하세요</h2>
+                                <Calendar
+                                    className="calender-custom"
+                                    selectRange={true}
+                                    onChange={setSelectedRange}
+                                    value={selectedRange}
+                                />
+                                <div className="modal-buttons">
+                                    <button className="back-button" onClick={handlePreviousStep}>
+                                    </button>
+                                    <button className="next-button" onClick={handleNextStep}>
+                                        다음
+                                    </button>
+                                </div>
+                            </>
+                        )}
+
+                        {step === 3 && (
+                            <div className="time-container">
+                                <h2>STEP 3: 여행 시간을 설정하세요</h2>
+                                <input
+                                    type="time"
+                                    value={startTime}
+                                    onChange={(e) => setStartTime(e.target.value)}
+                                />
+                                <input
+                                    type="time"
+                                    value={endTime}
+                                    onChange={(e) => setEndTime(e.target.value)}
+                                />
+                                <div className="modal-buttons">
+                                    <button className="back-button" onClick={handlePreviousStep}>
+                                    </button>
+                                    <button className="next-button" onClick={handleNextStep}>
+                                        다음
+                                    </button>
+                                </div>
                             </div>
-                        </>
-                    )}
-                
-                    {step === 5 && (
-                        <>
-                            <h2>모든 설정이 완료되었습니다!</h2>
-                            <p>잠시만 기다려 주세요...</p>
-                            <div className="modal-buttons">
-                                <button className="back-button" onClick={handlePreviousStep}>
-                                </button>
-                            </div>
-                        </>
-                    )}
-                </Modal>                
+                        )}
+
+                        {!isSolo && step === 4 && (
+                            <>
+                                <h2>STEP 4: 친구를 초대하세요</h2>
+                                <input
+                                    type="text"
+                                    placeholder="친구 아이디 입력"
+                                    value={friendId}
+                                    onChange={(e) => setFriendId(e.target.value)}
+                                />
+                                <button onClick={handleAddFriend}>친구 초대</button>
+                                <ul>
+                                    {validatedFriends.map((friend, index) => (
+                                        <li key={index}>{friend}</li> // 검증된 친구 아이디 표시
+                                    ))}
+                                </ul>
+                                <div className="modal-buttons">
+                                    <button className="back-button" onClick={handlePreviousStep}>
+                                    </button>
+                                    <button className="next-button" onClick={handleNextStep}>
+                                        다음
+                                    </button>
+                                </div>
+                            </>
+                        )}
+
+                        {step === 5 && (
+                            <>
+                                <h2>모든 설정이 완료되었습니다!</h2>
+                                <p>잠시만 기다려 주세요...</p>
+                                <div className="modal-buttons">
+                                    <button className="back-button" onClick={handlePreviousStep}>
+                                    </button>
+                                </div>
+                            </>
+                        )}
+                    </Modal>
                 )}
             </div>
         </div>
---
+++ tripMateApp/src/Services/tripService.js
@@ -0,0 +1,61 @@
+
+import axios from 'axios';
+const API_URL_MAKE_TRIP = 'https://www.daebak.store/trips';
+const token = localStorage.getItem('access_token');
+
+// 여행 생성 함수
+export const createTrip = async (title, selectedRange, startTime, endTime) => {
+    const startDate = selectedRange[0].toISOString().split('T')[0]; // 시작 날짜
+    const endDate = selectedRange[1].toISOString().split('T')[0]; // 종료 날짜
+
+    console.log(startDate, endDate);
+
+    const tripData = {
+        name: title,
+        start_date: startDate,
+        end_date: endDate,
+        start_time: startTime.replace(':', ''), // HHMM 형식으로 변환
+        end_time: endTime.replace(':', '') // HHMM 형식으로 변환
+    };
+
+    console.log("tripDate: " + tripData);
+
+    try {
+        const response = await axios.post(API_URL_MAKE_TRIP, tripData, {
+            headers: {
+                'Authorization': `${token}` // 인증 토큰을 헤더에 추가
+            }
+        });
+
+        // 반환된 값을 저장
+
+        return response.data; // API 응답 데이터 반환
+    } catch (error) {
+        throw new Error('여행 생성에 실패했습니다: ' + error.message); // 에러 처리
+    }
+};
+
+// 친구 초대 함수
+ export const inviteFriend = async (memberIds, tripId) => {
+    console.log("반환된 값 확인: ");
+    
+    console.log(tripId);
+
+    const memberData = {
+        memberIds: memberIds
+    }
+
+    console.log(memberData);
+
+    try {
+        const response = await axios.post(`https://www.daebak.store/trips/${tripId}/invite`, memberData, {
+            headers: {
+                'Authorization': `${token}` // 인증 토큰을 헤더에 추가
+            }
+        });
+        return response.data;
+    }
+    catch (error) {
+        throw new Error('친구 초대에 실패했습니다: ' + error.message); // 에러 처리
+    }
+};
